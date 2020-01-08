10f4b95b7a309fffbaa87eb7f5ad431f8b0271b7
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-08 16:17:22 +0000

    complete uppercaser ex.

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,12 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
-}
+  const uppercasedWords = this.words.map((word) => {
+    return word.toUpperCase();
+  })
+  return uppercasedWords;
+};
 
 module.exports = UpperCaser;
