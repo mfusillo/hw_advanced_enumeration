d7e2337ef674f459d8bd87e35282152d683691aa
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-08 18:15:32 +0000

    complete pangram-finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,16 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().replace(/[^a-zA-Z]/g, "");
 }
 
 PangramFinder.prototype.isPangram = function () {
+  // for(let i = 0; i < this.alphabet.length; i++){
+  //   return this.phrase.indexOf(this.alphabet[i]) !== -1
+  // }
 
-}
+  return this.alphabet.every((letter) => {
+    return this.phrase.indexOf(letter) !== -1
+  })
+};
 
 module.exports = PangramFinder;
