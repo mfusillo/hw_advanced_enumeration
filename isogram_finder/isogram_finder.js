e99d9e66ea6f7d59d95b4e76a5d060690b781316
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-09 08:31:56 +0000

    complete isogram exercise

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,12 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
 
+  let set = new Set(this.word);
+  const uniqueLetters = [...set];
+  return uniqueLetters.length === this.word.length;
 }
 
 module.exports = IsogramFinder;
